1fa6fbc56c13036ff407a28ea870502abf4987ec
ndemo-richard <ndemorich@gmail.com>
2021-09-26 00:10:46 +0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-a news website
+<h1>About the project</h1>
+This is a news website. As simple as it is. users are able to add blog post in real time. It supports image and video embedding.
+It is connect by aws which is used to search media files to headless wordpress backhead which is then displayed by nextjs frontend.
+
+<h2>Build with</h2>
+
+This website is build with:
+
+<ul>
+ <li>Nextjs</li>
+<li>wordpress</li>
+<ul>
+
+<h2>Licence</h2>
+
+Distributed under the MIT Licence
+
+<h2>Contact</h2>
+
+my email: [ndemorich@gmail.com](ndemorich@gmail.com)
+
+project Link: [https://dajudigital.vercel.app/](https://dajudigital.vercel.app/)
+
+<h2>Acknowledgements</h2>
+
+<ul>
+<li>vercel</li>
+<li>AWS</li>
+<li>wordpress</li>
+</ul>
--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -6,7 +6,26 @@ import styles from "../styles/Footer.module.scss";
 
 const Footer = props => (
   <div className={styles.Footer}>
-  <Link href="/" passHref>
+    <div className={styles.wrapper}>
+      <div className={styles.advert} ><h3>join 20,000+ people who get our news alerts</h3>
+        <form className="styles.form">
+          <input className={styles.input} type="email" placeholder="example@email.com" />
+          <input className={styles.input} type="submit" value="Subscribe"/>
+        </form>
+        <p>By signing up you agree to our Privacy Policy</p>
+      </div>
+      <div className={styles.about}>
+        <h2>DajuDigital</h2>
+        <p>We strive to bring you educative, entertaing and breaking news to make a diffence to the society.Get informed</p>
+        <h3>Learn more about us</h3>
+      </div>
+      <div className={styles.contact}>
+        <h2>Do you have any news!</h2>
+        <h3>contact us today</h3>
+      </div>
+    </div>
+
+    <Link href="/" passHref>
     <div className={styles.copyright}>
     copyright @ 2021
     </div>      
